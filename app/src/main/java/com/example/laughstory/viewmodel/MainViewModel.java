b43ded904ed6a739fcfac7cb4f0fca0efd15a18a
NamBin <nguyenhoainam.090801@gmail.com>
2021-04-13 15:34:46 +0700

    oki

--- app/src/main/java/com/example/laughstory/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/laughstory/viewmodel/MainViewModel.java
@@ -2,6 +2,7 @@ package com.example.laughstory.viewmodel;
 
 import android.util.Log;
 
+
 import androidx.lifecycle.ViewModel;
 
 import com.example.laughstory.App;
@@ -31,27 +32,27 @@ public class MainViewModel extends ViewModel {
         }
     }
 
-    public void initListStory(String fileName){
-        String story = CommonUtils.getInstance().getTextAs("data/" + fileName + ".txt");
-        // Toast.makeText(this,""+story,Toast.LENGTH_LONG).show();
-        if (story == null) return;
-
-        String[] items = story.split("','0'\\);");
-        if (items.length == 0) return;
-        listStory  =new ArrayList<>();
-
-        for (String text : items) {
-            int start = text.indexOf("\n", 2);
-            if(start==-1){
-                continue;
-            }
-            String name = text.substring(0, start).trim();
-            String content = text.substring(start+1);
-            //  Toast.makeText(this,"Name"+name,Toast.LENGTH_LONG).show();
-            listStory.add(new Story(name, content));
-        }
-        Log.i(TAG, "Get story success: "+listStory.size());
-    }
+//    public void initListStory(String fileName){
+//        String story = CommonUtils.getInstance().getTextAs("data/" + fileName + ".txt");
+//        // Toast.makeText(this,""+story,Toast.LENGTH_LONG).show();
+//        if (story == null) return;
+//
+//        String[] items = story.split("','0'\\);");
+//        if (items.length == 0) return;
+//        listStory  =new ArrayList<>();
+//
+//        for (String text : items) {
+//            int start = text.indexOf("\n", 2);
+//            if(start==-1){
+//                continue;
+//            }
+//            String name = text.substring(0, start).trim();
+//            String content = text.substring(start+1);
+//            //  Toast.makeText(this,"Name"+name,Toast.LENGTH_LONG).show();
+//            listStory.add(new Story(name, content));
+//        }
+//        Log.i(TAG, "Get story success: "+listStory.size());
+//    }
 
     public List<Topic> getListTopic() {
         return listTopic;
